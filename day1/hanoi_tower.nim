d057fc69d85a67de58674fa26cfd4ba03a3ab243
Antonis G <capoiosct@gmail.com>
2017-07-09 14:54:59 +0300

    Add comments

--- day1/hanoi_tower.nim
+++ day1/hanoi_tower.nim
@@ -6,8 +6,8 @@ proc hanoi(n: int, fr, to, spare: string) =
    if n == 1:
       echoMove(fr, to)
    else:
-      hanoi(n - 1, fr, spare, to)
-      hanoi(1, fr, to, spare)
-      hanoi(n - 1, spare, to, fr)
+      hanoi(n - 1, fr, spare, to) #1 [recursively] move N-1 disks from left to middle
+      hanoi(1, fr, to, spare)     #2 move largest disk from left to right
+      hanoi(n - 1, spare, to, fr) #3 [recursively] move N-1 disks from middle to right
 
 hanoi(3, "left", "right", "middle")
