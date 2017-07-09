96f9cf684909ab54c07b0b257f397a8793927642
Antonis G <capoiosct@gmail.com>
2017-07-09 14:52:36 +0300

    I better understand this version

--- day1/hanoi_tower.nim
+++ day1/hanoi_tower.nim
@@ -3,9 +3,11 @@ proc echoMove(fr, to: string) {.inline.} =
    echo "move from ", fr, " to ", to
 
 proc hanoi(n: int, fr, to, spare: string) =
-   if n > 0:
-      hanoi(n - 1, fr, spare, to)
+   if n == 1:
       echoMove(fr, to)
+   else:
+      hanoi(n - 1, fr, spare, to)
+      hanoi(1, fr, to, spare)
       hanoi(n - 1, spare, to, fr)
 
 hanoi(3, "left", "right", "middle")
