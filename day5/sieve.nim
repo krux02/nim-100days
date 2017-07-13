19e6b1fab094b4b83a6f42f44fc1dda73435d76a
Antonis G <capoiosct@gmail.com>
2017-07-13 21:36:52 +0300

    Add working sieve of eratosthenes impl

--- day5/sieve.nim
+++ day5/sieve.nim
@@ -1,11 +1,16 @@
-proc eratosthenes(n: Natural): int =
-   let n = (n + 1) / 2
-   
-   
-   while i < n:
-      if p[i] > 0:
-         p[j * j / 2] = 0
-      i.inc(1)
-      j.inc(2)
-   
-   sum(p)
+import math
+
+
+proc eratosthenes(n: Natural): seq[int8] =
+  result = newSeq[int8](n + 1)
+  result[0] = 1
+  result[1] = 1
+
+  let slimit = sqrt(n.float).int
+  for i in 0 .. slimit:
+    if result[i] == 0:
+      for j in countup(i * i, n, i):
+        result[j] = 1
+
+
+echo eratosthenes(100)
