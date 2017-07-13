350cf150740b55a5ce9e72d5c823baeb454e5227
Antonis G <capoiosct@gmail.com>
2017-07-13 21:47:04 +0300

    Will fix it eventually

--- day5/sieve.nim
+++ day5/sieve.nim
@@ -1,16 +1,18 @@
 import math
 
 
-proc eratosthenes(n: Natural): seq[int8] =
-  result = newSeq[int8](n + 1)
-  result[0] = 1
-  result[1] = 1
+proc eratosthenes(n: Natural): int =
+   var p = newSeq[int8](n + 1)
+   p[0] = 1
+   p[1] = 1
 
-  let slimit = sqrt(n.float).int
-  for i in 0 .. slimit:
-    if result[i] == 0:
-      for j in countup(i * i, n, i):
-        result[j] = 1
+   let slimit = sqrt(n.float).int
+   for i in 0 .. slimit:
+      if p[i] == 0:
+         for j in countup(i * i, n, i):
+            p[j] = 1
 
+   for i in p:
+      result.inc i
 
-echo eratosthenes(100)
+echo eratosthenes(1_000_000)
