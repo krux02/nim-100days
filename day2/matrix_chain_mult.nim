1db7bdbf58f92fc39a84e1c4bc008805a7f614db
Antonis G <capoiosct@gmail.com>
2017-12-14 16:52:57 +0200

    Optimize solution

--- day2/matrix_chain_mult.nim
+++ day2/matrix_chain_mult.nim
@@ -1,37 +1,32 @@
-import tables
-
 
 proc mult(x: openArray[(string, int, int)]): tuple[cost: int, order: string, rows, cols: int] =
-   let n = len(x) - 1
-
-   var aux = initTable[(int, int), (int, string, int, int)]()
-   for i in 0 .. n:
+   # return the linear index of row, column
+   template `[]`(s: seq; a, b: int): untyped = s[a * n + b]
+   template `[]=`(s: seq; a, b: int; val: typed) = s[a * n + b] = val
+   let n = len(x)
+   var aux = newSeq[(int, string, int, int)](n * n)
+   for i in 0 ..< n:
       let (tname, trow, tcol) = x[i]
       # single matrix chain has zero cost
-      aux[(i, i)] = (0, tname, trow, tcol)
-
+      aux[i, i] = (0, tname, trow, tcol)
    # i: length of subchain
-   for i in 1 .. n:
+   for i in 1 ..< n:
       # j: starting index of subchain
-      for j in 0 .. n - i:
+      for j in 0 ..< n - i:
          var best = high(int)
-
          # k: splitting point of subchain
-         for k in j .. < j + i:
+         for k in j ..< j + i:
             # multiply subchains at splitting point
             let
-               (lcost, lname, lrow, lcol) = aux[(j, k)]
-               (rcost, rname, rrow, rcol) = aux[(k + 1, j + i)]
+               (lcost, lname, lrow, lcol) = aux[j, k]
+               (rcost, rname, rrow, rcol) = aux[k + 1, j + i]
                cost = lcost + rcost + lrow * lcol * rcol
                tvar = "(" & lname & rname & ")"
-
             # pick the best one
             if cost < best:
                best = cost
-               aux[(j, j + i)] = (cost, tvar, lrow, rcol)
-
-   aux[(0, n)]
-
+               aux[j, j + i] = (cost, tvar, lrow, rcol)
+   result = aux[0, n - 1]
 
 
 echo mult([("A", 10, 20), ("B", 20, 30), ("C", 30, 40)])
