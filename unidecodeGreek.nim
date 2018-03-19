a6a1c2b7e9a6c5a9c1787df1146c0fa3872636fd
Antonis G <capoiosct@gmail.com>
2018-03-19 15:22:42 +0200

    Add translitarate

--- unidecodeGreek.nim
+++ unidecodeGreek.nim
@@ -1,8 +1,109 @@
-import unicode, sequtils, strutils
+import unicode, strutils
 
 const
+   elotMapping = { # digraphs are on top of their first letter
+      ";":  "?",
+      "Ά":  "A",
+      "·":  ";",
+      "Έ":  "E",
+      "Ή":  "I",
+      "Ί":  "I",
+      "Όυ": "Oy",
+      "Ό":  "O",
+      "Ύ":  "Y",
+      "Ώ":  "O",
+      "ΐ":  "i",
+      "Αι": "Ai",
+      "Αυ": "Au",
+      "Α":  "A",
+      "Β":  "V",
+      "Γκ": "Gk",
+      "Γ":  "G",
+      "Δ":  "D",
+      "Ει": "Ei",
+      "Ευ": "Eu",
+      "Ε":  "E",
+      "Ζ":  "Z",
+      "Ηυ": "Iy",
+      "Η":  "I",
+      "Θ":  "Th",
+      "Ι":  "I",
+      "Κ":  "K",
+      "Λ":  "L",
+      "Μπ": "Mp",
+      "Μ":  "M",
+      "Ντ": "Nt",
+      "Ν":  "N",
+      "Ξ":  "X",
+      "Οι": "Oi",
+      "Ου": "Ou",
+      "Οϋ": "Oy",
+      "Ο":  "O",
+      "Π":  "P",
+      "Ρ":  "R",
+      "Σ":  "S",
+      "Τ":  "T",
+      "Υι": "Yi",
+      "Υ":  "Y",
+      "Φ":  "F",
+      "Χ":  "Ch",
+      "Ψ":  "Ps",
+      "Ωυ": "Oy",
+      "Ω":  "O",
+      "Ϊ":  "I",
+      "Ϋ":  "Y",
+      "ά":  "a",
+      "έ":  "e",
+      "ή":  "i",
+      "ί":  "i",
+      "ΰ":  "u",
+      "αυ": "au",
+      "α":  "a",
+      "β":  "v",
+      "γγ": "gg",
+      "γκ": "gk",
+      "γξ": "gx",
+      "γχ": "gch",
+      "γ":  "g",
+      "δ":  "d",
+      "ευ": "eu",
+      "ε":  "e",
+      "ζ":  "z",
+      "ηυ": "iy",
+      "η":  "i",
+      "θ":  "th",
+      "ι":  "i",
+      "κ":  "k",
+      "λ":  "l",
+      "μ":  "m",
+      "ντ": "nt",
+      "ν":  "n",
+      "ξ":  "x",
+      "οι": "oi",
+      "ου": "ou",
+      "οϋ": "oy",
+      "ο":  "o",
+      "π":  "p",
+      "ρ":  "r",
+      "ς":  "s",
+      "σ":  "s",
+      "τ":  "t",
+      "υι": "yi",
+      "υ":  "y",
+      "φ":  "f",
+      "χ":  "ch",
+      "ψ":  "ps",
+      "ωυ": "oy",
+      "ω":  "o",
+      "ϊ":  "i",
+      "ϋ":  "u",
+      "όυ": "oy",
+      "ό":  "o",
+      "ύ":  "u",
+      "ώ":  "o"}
+
    englishMapping = [
-      "?",  # ;
+      "?",  # ;
       "",   #
       "",   #
       "",   #
@@ -92,8 +193,16 @@ proc unidecode*(s: string): string =
       else:
          result.add($r)
 
+proc translitarate*(s: string): string {.inline.} =
+   multiReplace(s, elotMapping)
+
 when isMainModule:
    assert unidecode("Ελληνική Δημοκρατία") == "Elliniki Dimokratia"
    assert unidecode("Ελευθερία") == "Eleutheria"
    assert unidecode("Ευαγγέλιο") == "Euaggelio"
    assert unidecode("των υιών") == "ton uion"
+
+   assert translitarate("Ελληνική Δημοκρατία") == "Elliniki Dimokratia"
+   assert translitarate("Ελευθερία") == "Eleutheria"
+   assert translitarate("Ευαγγέλιο") == "Euaggelio"
+   assert translitarate("των υιών") == "ton yion"
