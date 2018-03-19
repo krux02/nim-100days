3efeb17da8a57bb6ddfc588414e32c9f427f250c
Antonis G <capoiosct@gmail.com>
2018-03-19 14:34:21 +0200

    much better

--- unidecodeGreek.nim
+++ unidecodeGreek.nim
@@ -1,81 +1,23 @@
 import unicode, sequtils, strutils
 
 const
-   greekLetters = mapLiterals([
-      0x0386,  # Ά
-      0x0388,  # Έ
-      0x0389,  # Ή
-      0x038a,  # Ί
-      0x038c,  # Ό
-      0x038e,  # Ύ
-      0x038f,  # Ώ
-      0x0390,  # ΐ
-      0x0391,  # Α
-      0x0392,  # Β
-      0x0393,  # Γ
-      0x0394,  # Δ
-      0x0395,  # Ε
-      0x0396,  # Ζ
-      0x0397,  # Η
-      0x0398,  # Θ
-      0x0399,  # Ι
-      0x039a,  # Κ
-      0x039b,  # Λ
-      0x039c,  # Μ
-      0x039d,  # Ν
-      0x039e,  # Ξ
-      0x039f,  # Ο
-      0x03a0,  # Π
-      0x03a1,  # Ρ
-      0x03a3,  # Σ
-      0x03a4,  # Τ
-      0x03a5,  # Υ
-      0x03a6,  # Φ
-      0x03a7,  # Χ
-      0x03a8,  # Ψ
-      0x03a9,  # Ω
-      0x03aa,  # Ϊ
-      0x03ab,  # Ϋ
-      0x03ac,  # ά
-      0x03ad,  # έ
-      0x03ae,  # ή
-      0x03af,  # ί
-      0x03b1,  # α
-      0x03b2,  # β
-      0x03b3,  # γ
-      0x03b4,  # δ
-      0x03b5,  # ε
-      0x03b6,  # ζ
-      0x03b7,  # η
-      0x03b8,  # θ
-      0x03b9,  # ι
-      0x03ba,  # κ
-      0x03bb,  # λ
-      0x03bc,  # μ
-      0x03bd,  # ν
-      0x03be,  # ξ
-      0x03bf,  # ο
-      0x03c0,  # π
-      0x03c1,  # ρ
-      0x03c3,  # σ
-      0x03c4,  # τ
-      0x03c5,  # υ
-      0x03c6,  # φ
-      0x03c7,  # χ
-      0x03c8,  # ψ
-      0x03c9,  # ω
-      0x03ca,  # ϊ
-      0x03cb,  # ϋ
-      0x03cc,  # ό
-      0x03cd,  # ύ
-      0x03ce], Rune) # ώ
-
    englishMapping = [
+      "?",  # ;
+      "",   #
+      "",   #
+      "",   #
+      "",   #
+      "",   #
+      "",   #
+      "",   #
       "A",  # Ά
+      ";",  # ·
       "E",  # Έ
       "I",  # Ή
       "I",  # Ί
+      "",   #
       "O",  # Ό
+      "",   #
       "Y",  # Ύ
       "O",  # Ώ
       "i",  # ΐ
@@ -96,6 +38,7 @@ const
       "O",  # Ο
       "P",  # Π
       "R",  # Ρ
+      "",   #
       "S",  # Σ
       "T",  # Τ
       "Y",  # Υ
@@ -109,6 +52,7 @@ const
       "e",  # έ
       "i",  # ή
       "i",  # ί
+      "u",  # ΰ
       "a",  # α
       "v",  # β
       "g",  # γ
@@ -127,6 +71,7 @@ const
       "p",  # π
       "r",  # ρ
       "s",  # σ
+      "s",  # ς
       "t",  # τ
       "u",  # υ
       "f",  # φ
@@ -139,24 +84,11 @@ const
       "u",  # ύ
       "o"]  # ώ
 
-proc binaryRuneSearch(a: openArray[Rune], key: Rune): int =
-   # binary search for `key` in `a`. Returns -1 if not found.
-   var b = len(a)
-   while result < b:
-      var mid = (result + b) div 2
-      if a[mid] <% key:
-         result = mid + 1
-      else:
-         b = mid
-   if result >= len(a) or a[result] != key:
-      result = -1
-
 proc unidecode*(s: string): string =
    result = newStringOfCap(s.len * 2 div 3) # just a guess
    for r in runes(s):
-      var i = binaryRuneSearch(greekLetters, r)
-      if i >= 0:
-         result.add(englishMapping[i])
+      if int32(r) >= 0x037e'i32 and int32(r) <= 0x03ce'i32:
+         result.add(englishMapping[int(r) - 0x037e])
       else:
          result.add($r)
 
